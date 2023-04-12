23affa0649b3af8da2f52aa33040be548791f336
r.iappelli <riccardo.iappelli@staersistemi.com>
2023-04-12 15:45:21 +0200

    one line

--- 2_conta_righe/contaRighe.sh
+++ 2_conta_righe/contaRighe.sh
@@ -2,6 +2,4 @@
 # Script la stampa di n. righe e path con l'aggionta di '#!' all'inizio 
 
 # Cerco il file dentro la cartella & stampo n.righe e #!/$path_file
-find $1 -type f -exec wc -l {} \; \
-| awk '{gsub(" "," #!");}1' \      
-| awk '{$1++; print $0}'            
+find $1 -type f -exec wc -l {} \; | awk '{gsub(" "," #!");}1' | awk '{$1++; print $0}'
