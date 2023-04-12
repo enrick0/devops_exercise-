49a7c07be3bf2970a8d8bd1b0e80900dcb98348c
r.iappelli <riccardo.iappelli@staersistemi.com>
2023-04-12 15:40:08 +0200

    fix: line counter start to 1

--- 2_conta_righe/contaRighe.sh
+++ 2_conta_righe/contaRighe.sh
@@ -2,4 +2,6 @@
 # Script la stampa di n. righe e path con l'aggionta di '#!' all'inizio 
 
 # Cerco il file dentro la cartella & stampo n.righe e #!/$path_file
-find $1 -type f -exec echo {} \; 
+find $1 -type f -exec wc -l {} \; \
+    | awk '{gsub(" "," #!");}1' \
+    | awk '{$1++; print $0}'
