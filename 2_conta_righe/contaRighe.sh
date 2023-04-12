3379cb8b1278c31a4dc98e6baaf5a101a4df51a6
r.iappelli <riccardo.iappelli@staersistemi.com>
2023-04-12 15:10:29 +0200

    fix: argomento cli

--- 2_conta_righe/contaRighe.sh
+++ 2_conta_righe/contaRighe.sh
@@ -2,4 +2,4 @@
 # Script la stampa di n. righe e path con l'aggionta di '#!' all'inizio 
 
 # Cerco il file dentro la cartella & stampo n.righe e #!/$path_file
-find $3 -type f -exec wc -l {} \; 
+find $1 -type f -exec echo {} \; 
